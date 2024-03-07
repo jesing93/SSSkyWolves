c364356150eefa1edde060a74cd4b5226fa94cf5
HugoGonzaMora <hugoma21@gmail.com>
2024-03-07 14:34:09 +0100

    MenuPausaCasiFinal

--- Assets/Scripts/UI/Visuals/PauseMenu.cs
+++ Assets/Scripts/UI/Visuals/PauseMenu.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
     //Region dedicated to the different Variables.
     #region Variables
-    private Image pausePanel;
-    private Image optionPanel;
-    private Image creditsPanel;
+    public GameObject pausePanel;
+    public GameObject optionPanel;
+    public GameObject creditsPanel;
     #endregion
 
     //Region deidcated to the different Getters/Setters.
@@ -21,15 +22,66 @@ public class PauseMenu : MonoBehaviour
     #region Unity Functions
     private void Start()
     {
-        pausePanel.enabled = false;
-        optionPanel.enabled = false;
-        creditsPanel.enabled = false;
+        pausePanel.SetActive(false);
+        optionPanel.SetActive(false);
+        creditsPanel.SetActive(false);
     }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            pausePanel.enabled = true;
+            CambiarPanel(pausePanel);
+            Time.timeScale = 0.0f;
+        }
+    }
+
+    public void OnClickContinue()
+    {
+        CambiarPanel();
+        Time.timeScale = 1.0f;
+    }
+    public void OnClickOpciones()
+    {
+        CambiarPanel(optionPanel);
+    }
+
+    public void OnClickMenu()
+    {
+        SceneManager.LoadScene("MainTitle");
+        Time.timeScale = 1.0f;
+    } 
+    
+    public void OnClickReiniciar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1.0f;
+    }
+
+    public void OnClickCreditos()
+    {
+        CambiarPanel(creditsPanel);
+    }
+
+    public void OnClickVolver()
+    {
+        CambiarPanel(pausePanel);
+    }
+
+    public void OnClickSalir()
+    {
+        Application.Quit();
+        Debug.Log("ME CIERRO A-");
+    }
+
+    void CambiarPanel(GameObject panel = null)
+    {
+        pausePanel.SetActive(false);
+        optionPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+
+        if(panel != null)
+        { 
+            panel.SetActive(true);
         }
     }
     #endregion
