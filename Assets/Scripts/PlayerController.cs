9c1daa923f750a04aab891690193573798e58d57
Ramon <jblapei796@g.educaand.es>
2024-03-11 09:54:08 +0100

    Missing using

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
