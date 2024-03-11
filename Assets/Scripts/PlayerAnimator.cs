8978ed8a21a64c2ab1c6467ff392c0afb671e618
Sienfi <150896873+Sienfi@users.noreply.github.com>
2024-03-11 09:04:31 +0100

    Caminata arreglada y prefabs de Lobos

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -7,13 +7,14 @@ public class PlayerAnimator : MonoBehaviour
 {
     [Header("Legs Features")]
     [SerializeField] private Vector3 legsConstraints;
-    [SerializeField] private Vector3 legsOffset;
     [SerializeField] private Vector3 legsSeparation;
+    [SerializeField] private Vector3 legsOffset;
     [SerializeField] private float legsFlexion;
 
     [Header("Adaptation")]
     [SerializeField] private float anticipation;
     [SerializeField] private LayerMask groundLayer;
+    private float speed;
 
     [Header("Walk Animation")]
     [SerializeField] private float stepHeigh;
@@ -39,9 +40,11 @@ public class PlayerAnimator : MonoBehaviour
 
     // Setters
     private Vector2 moveInput = Vector2.zero;
+    private Vector3 velocity = Vector2.zero;
+    private bool isStillGrounded = false;
     public Vector2 MoveInput { set { moveInput = value; } }
-    private float speed = 0;
-    public float Speed { set { speed = value; } }
+    public Vector3 Velocity { set { velocity = value; } }
+    public bool IsStillGrounded { set { isStillGrounded = value; } }
 
     // Walk Cycle
     private int side = -1;
@@ -51,69 +54,71 @@ public class PlayerAnimator : MonoBehaviour
         StartCoroutine(WalkAnim());
     }
 
+    private void Update()
+    {
+        if (isStillGrounded) speed = moveInput.magnitude / 4;
+        else speed = Mathf.Abs(velocity.y);
+    }
+
     // Comprueba la altura local a la que se debe apoyar cada pata y desliza los valores en función a la velocidad. 
     private void CheckGround()
     {
-        Ray leftHandUpRay = new Ray(transform.position + wolfModel.transform.TransformDirection(new Vector3(leftHand.localPosition.x, legsConstraints.y, leftHand.localPosition.z )) + transform.forward * anticipation, wolfModel.transform.TransformDirection(Vector3.down));
+        Ray leftHandUpRay = new Ray(wolfModel.position + wolfModel.TransformDirection(new Vector3(leftHand.localPosition.x, legsConstraints.y, leftHand.localPosition.z )) + transform.forward * anticipation, wolfModel.TransformDirection(Vector3.down));
         RaycastHit leftHandUpHit;
-        Ray rightHandUpRay = new Ray(transform.position + wolfModel.transform.TransformDirection(new Vector3(rightHand.localPosition.x, legsConstraints.y, rightHand.localPosition.z )) + transform.forward * anticipation, wolfModel.transform.TransformDirection(Vector3.down));
+        Ray rightHandUpRay = new Ray(wolfModel.position + wolfModel.TransformDirection(new Vector3(rightHand.localPosition.x, legsConstraints.y, rightHand.localPosition.z )) + transform.forward * anticipation, wolfModel.TransformDirection(Vector3.down));
         RaycastHit rightHandUpHit;
-        Ray leftFootUpRay = new Ray(transform.position + wolfModel.transform.TransformDirection(new Vector3(leftFoot.localPosition.x, legsConstraints.y, leftFoot.localPosition.z )) + transform.forward * anticipation, wolfModel.transform.TransformDirection(Vector3.down));
+        Ray leftFootUpRay = new Ray(wolfModel.position + wolfModel.TransformDirection(new Vector3(leftFoot.localPosition.x, legsConstraints.y, leftFoot.localPosition.z )) + transform.forward * anticipation, wolfModel.TransformDirection(Vector3.down));
         RaycastHit leftFootUpHit;
-        Ray rightFootUpRay = new Ray(transform.position + wolfModel.transform.TransformDirection(new Vector3(rightFoot.localPosition.x, legsConstraints.y, rightFoot.localPosition.z )) + transform.forward * anticipation, wolfModel.transform.TransformDirection(Vector3.down));
+        Ray rightFootUpRay = new Ray(wolfModel.position + wolfModel.TransformDirection(new Vector3(rightFoot.localPosition.x, legsConstraints.y, rightFoot.localPosition.z )) + transform.forward * anticipation, wolfModel.TransformDirection(Vector3.down));
         RaycastHit rightFootUpHit;
 
-        Ray leftHandDownRay = new Ray(transform.position + wolfModel.transform.TransformDirection(new Vector3(leftHand.localPosition.x, 0.005f, leftHand.localPosition.z)), wolfModel.transform.TransformDirection(Vector3.down));
+        Ray leftHandDownRay = new Ray(wolfModel.position + wolfModel.TransformDirection(new Vector3(leftHand.localPosition.x, 0.005f, leftHand.localPosition.z)), wolfModel.TransformDirection(Vector3.down));
         RaycastHit leftHandDownHit;
-        Ray rightHandDownRay = new Ray(transform.position + wolfModel.transform.TransformDirection(new Vector3(rightHand.localPosition.x, 0.005f, rightHand.localPosition.z)), wolfModel.transform.TransformDirection(Vector3.down));
+        Ray rightHandDownRay = new Ray(wolfModel.position + wolfModel.TransformDirection(new Vector3(rightHand.localPosition.x, 0.005f, rightHand.localPosition.z)), wolfModel.TransformDirection(Vector3.down));
         RaycastHit rightHandDownHit;
-        Ray leftFootDownRay = new Ray(transform.position + wolfModel.transform.TransformDirection(new Vector3(leftFoot.localPosition.x, 0.005f, leftFoot.localPosition.z)), wolfModel.transform.TransformDirection(Vector3.down));
+        Ray leftFootDownRay = new Ray(wolfModel.position + wolfModel.TransformDirection(new Vector3(leftFoot.localPosition.x, 0.005f, leftFoot.localPosition.z)), wolfModel.TransformDirection(Vector3.down));
         RaycastHit leftFootDownHit;
-        Ray rightFootDownRay = new Ray(transform.position + wolfModel.transform.TransformDirection(new Vector3(rightFoot.localPosition.x, 0.005f, rightFoot.localPosition.z)), wolfModel.transform.TransformDirection(Vector3.down));
+        Ray rightFootDownRay = new Ray(wolfModel.position + wolfModel.TransformDirection(new Vector3(rightFoot.localPosition.x, 0.005f, rightFoot.localPosition.z)), wolfModel.TransformDirection(Vector3.down));
         RaycastHit rightFootDownHit;
 
         // Left Hand
         if (Physics.Raycast(leftHandUpRay, out leftHandUpHit, legsConstraints.y, groundLayer) && leftHandUpHit.distance > 0.005f)
         {
-            leftHandDistance = Mathf.MoveTowards(leftHandDistance, legsConstraints.y - leftHandUpHit.distance, Time.deltaTime * speed/2);
+            leftHandDistance = Mathf.MoveTowards(leftHandDistance, legsConstraints.y - leftHandUpHit.distance, Time.deltaTime * speed);
         }else if (Physics.Raycast(leftHandDownRay, out leftHandDownHit, legsConstraints.y, groundLayer))
         {
-            leftHandDistance = Mathf.MoveTowards(leftHandDistance, - leftHandDownHit.distance, Time.deltaTime * speed / 2);
+            leftHandDistance = Mathf.MoveTowards(leftHandDistance, - leftHandDownHit.distance, Time.deltaTime * speed);
         }
-        else leftHandDistance = Mathf.MoveTowards(leftHandDistance, 0, Time.deltaTime * speed / 2);
+        else leftHandDistance = Mathf.MoveTowards(leftHandDistance, 0, Time.deltaTime * speed);
         // Right Hand
         if (Physics.Raycast(rightHandUpRay, out rightHandUpHit, legsConstraints.y, groundLayer) && rightHandUpHit.distance > 0.005f)
         {
-            rightHandDistance = Mathf.MoveTowards(rightHandDistance, legsConstraints.y - rightHandUpHit.distance, Time.deltaTime * speed/2);
+            rightHandDistance = Mathf.MoveTowards(rightHandDistance, legsConstraints.y - rightHandUpHit.distance, Time.deltaTime * speed);
         }else if (Physics.Raycast(rightHandDownRay, out rightHandDownHit, legsConstraints.y, groundLayer))
         {
-            rightHandDistance = Mathf.MoveTowards(rightHandDistance, -rightHandDownHit.distance, Time.deltaTime * speed / 2);
+            rightHandDistance = Mathf.MoveTowards(rightHandDistance, -rightHandDownHit.distance, Time.deltaTime * speed);
         }
-        else rightHandDistance = Mathf.MoveTowards(rightHandDistance, 0, Time.deltaTime * speed / 2);
+        else rightHandDistance = Mathf.MoveTowards(rightHandDistance, 0, Time.deltaTime * speed);
         // Left Foot
         if (Physics.Raycast(leftFootUpRay, out leftFootUpHit, legsConstraints.y, groundLayer) && leftFootUpHit.distance > 0.005f)
         {
-            Debug.DrawRay(transform.position + wolfModel.transform.TransformDirection(new Vector3(leftFoot.localPosition.x, legsConstraints.y, leftFoot.localPosition.z + anticipation)), wolfModel.transform.TransformDirection(Vector3.down), Color.red, Time.deltaTime);
-
-            leftFootDistance = Mathf.MoveTowards(leftFootDistance, legsConstraints.y - leftFootUpHit.distance, Time.deltaTime * speed/2);
+            leftFootDistance = Mathf.MoveTowards(leftFootDistance, legsConstraints.y - leftFootUpHit.distance, Time.deltaTime * speed);
         }
         else if (Physics.Raycast(leftFootDownRay, out leftFootDownHit, legsConstraints.y, groundLayer))
         {
-            Debug.DrawRay(transform.position + wolfModel.transform.TransformDirection(new Vector3(leftFoot.localPosition.x, 0.05f, leftFoot.localPosition.z)), wolfModel.transform.TransformDirection(Vector3.down), Color.green, Time.deltaTime);
-
-            leftFootDistance = Mathf.MoveTowards(leftFootDistance, -leftFootDownHit.distance, Time.deltaTime * speed / 2);
+            leftFootDistance = Mathf.MoveTowards(leftFootDistance, -leftFootDownHit.distance, Time.deltaTime * speed);
         }
-        else leftFootDistance = Mathf.MoveTowards(leftFootDistance, 0, Time.deltaTime * speed / 2);
+        else leftFootDistance = Mathf.MoveTowards(leftFootDistance, 0, Time.deltaTime * speed);
         // Right Foot
         if (Physics.Raycast(rightFootUpRay, out rightFootUpHit, legsConstraints.y, groundLayer) && rightFootUpHit.distance > 0.005f)
         {
-            rightFootDistance = Mathf.MoveTowards(rightFootDistance, legsConstraints.y - rightFootUpHit.distance, Time.deltaTime * speed/2);
+            rightFootDistance = Mathf.MoveTowards(rightFootDistance, legsConstraints.y - rightFootUpHit.distance, Time.deltaTime * speed);
         }
         else if (Physics.Raycast(rightFootDownRay, out rightFootDownHit, legsConstraints.y, groundLayer))
         {
-            rightFootDistance = Mathf.MoveTowards(rightFootDistance, -rightFootDownHit.distance, Time.deltaTime * speed / 2);
+            rightFootDistance = Mathf.MoveTowards(rightFootDistance, -rightFootDownHit.distance, Time.deltaTime * speed);
         }
-        else rightFootDistance = Mathf.MoveTowards(rightFootDistance, 0, Time.deltaTime * speed / 2);
+        else rightFootDistance = Mathf.MoveTowards(rightFootDistance, 0, Time.deltaTime * speed);
         
     }
 
@@ -123,8 +128,9 @@ public class PlayerAnimator : MonoBehaviour
         float lessDistance = Mathf.Min(leftHandDistance, rightHandDistance, leftFootDistance, rightFootDistance);
         float greatestDistance = Mathf.Max(leftHandDistance, rightHandDistance, leftFootDistance, rightFootDistance);
 
-        armatureOffset = Vector3.MoveTowards(armature.localPosition, new Vector3(0, (lessDistance + greatestDistance) / 2 - legsFlexion, 0), Time.deltaTime * speed / 2);
-
+        //armatureOffset = Vector3.MoveTowards(armature.localPosition, new Vector3(0, (lessDistance + greatestDistance) / 2 - legsFlexion, 0), Time.deltaTime * planeVelocity / 2 + Time.deltaTime * Mathf.Abs(verticalVelocity));
+        armatureOffset = Vector3.MoveTowards(armature.localPosition, new Vector3(0, (lessDistance + greatestDistance) / 2 - legsFlexion, 0), speed * Time.fixedDeltaTime);
+        
         armature.localPosition = armatureOffset;
 
         leftHandOrigin = legsOffset + new Vector3(-legsSeparation.x / 2, legsSeparation.y / 2, legsSeparation.z / 2) + Vector3.up * (leftHandDistance) - armatureOffset;
@@ -140,7 +146,7 @@ public class PlayerAnimator : MonoBehaviour
         {
             Vector3 lOffset = Vector3.forward * legsConstraints.z * side;
 
-            for (float i = 0; i < 1; i += Time.deltaTime * speed)
+            for (float i = 0; i < 1; i += Time.deltaTime * new Vector3(velocity.x, 0, velocity.z).magnitude)
             {
                 CheckGround();
                 SetOrigins();
